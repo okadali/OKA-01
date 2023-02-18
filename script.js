c984ebe13c5ee216ce80d447346a6f631a6b51c2
okadali <okadali@hotmail.com>
2023-02-18 19:15:35 +0300

    Keyboard added

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-let selectedWaveform = 0;
-
-
-let waveformSlider = document.getElementById("waveformTypeSlider")
-waveformSlider.oninput = function() {
-    selectedWaveform = this.value;
-    document.getElementById("waveformTypeName").innerHTML = WAVEFORMS[selectedWaveform];
+const NOTES = {
+    'c-4' : 261.63,
+    'c#4' : 277.18,
+    'd-4' : 293.67,
+    'd#4' : 311.13,
+    'e-4' : 329.63,
+    'f-4' : 349.23,
+    'f#4' : 370.23,
+    'g-4' : 392.00,
+    'g#4' : 415.30,
+    'a-4' : 440.00,
+    'a#4' : 466.16,
+    'b-4' : 493.88,
+    'c-5' : 523.25,
+    'c#5' : 554.37,
+    'd-5' : 587.33,
+    'd#5' : 622.25,
+    'e-5' : 659.26,
 }
 
-document.querySelector("#play").addEventListener('click',() => {
-    const actx = new (AudioContext || webkitAudioContext)();
-    if(!actx) throw 'Not Supported';
-    const osc = actx.createOscillator();
-    osc.type = WAVEFORMS[selectedWaveform];
-    osc.frequency.value = 440; //Hz
-    osc.connect(actx.destination);
-    osc.start();
-    osc.stop(actx.currentTime + 2);
+const actx = new (AudioContext || webkitAudioContext)();
+if(!actx) throw 'Not Supported';
+
+let osc;
+
+document.querySelectorAll('div[data-note]').forEach((button) => {
+    button.addEventListener('mousedown', () => {
+        osc = actx.createOscillator();
+        osc.type = "sawtooth";
+        osc.frequency.value = NOTES[button.dataset.note];
+        osc.connect(actx.destination);
+        osc.start();
+
+    })
+
+    button.addEventListener('mouseup', () => {osc.stop()});
 })
 
-const WAVEFORMS = [
-    "sine",
-    "square",
-    "sawtooth",
-    "triangle"
-]
+
+
+
